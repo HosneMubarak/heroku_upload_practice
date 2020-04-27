b4ba75c2d797421553aae93a1cb0f96456bab4a5
Hosne Mubarak <h.m.tasmir.cse@gmail.com>
2020-04-28 02:04:28 +0600

    add new adress database

--- adress_book/models.py
+++ adress_book/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=11)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=20)
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.name
